2c45a0ff247b012f58f416f2bc18e9a409910bde
prince <princekumar.dev26@gmail.com>
2026-08-30 21:37:09 +0530

    Prepare backend for Vercel

--- server/server.js
+++ server/server.js
@@ -21,5 +21,8 @@ app.get('/', (req, res)=> res.send("API is working "))
 
 const PORT = process.env.PORT || 3000;
 
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+export default app;
